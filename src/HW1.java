74ca333edefd93142e0fd9890c6a4ac893b1683d
Kristine Necajeva <kristinenecajeva@inbox.lv>
2022-11-16 21:51:29 +0200

    HW1

--- src/HW1.java
+++ src/HW1.java
@@ -1,2 +1,19 @@
+import java.util.Scanner;
+
 public class HW1 {
+    public static void main(String[] args) {
+
+        // Using scanner ask user to enter name and year of birth
+        // Print out:
+        // Input: John, 2000
+        // "Your name is John and you are 22 years old"
+
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Please enter your name");
+        String name = scanner.nextLine();
+        System.out.println("Please enter yor age");
+        int age = scanner.nextInt();
+        System.out.println("Your name is " + name + " and you are " + age + " years old");
+
+    }
 }
