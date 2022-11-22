09509cf69717d91e584983e5fe8007ff2fb22881
Kristine Necajeva <kristinenecajeva@inbox.lv>
2022-11-22 22:13:53 +0200

    HW3 and HW4

--- src/homeworks/HW3.java
+++ src/homeworks/HW3.java
@@ -1,5 +1,7 @@
 package homeworks;
 
+import java.util.Scanner;
+
 public class HW3 {
     public static void main(String[] args) {
         /*      Write an application, that will read one number from user (of type int)
@@ -9,7 +11,22 @@ public class HW3 {
                Your application should read one number (int) and print false or true on the screen,
                 according to instructions above.*/
 
+        Scanner scanner = new Scanner(System.in);
 
+        System.out.println("Enter a grade1");
+        int grade1 =scanner.nextInt();
+
+        System.out.println("Enter a grade2");
+        int grade2 = scanner.nextInt();
+
+        if ((grade1 >= 90 && grade1 <= 110) && (grade2 >= 90 && grade2 <= 110)) {
+            System.out.println("True");
+
+        } else {
+            System.out.println("False");
+        }
+
+        System.out.println("Max " + Math.max(grade1,grade2));
 
 
 
--- src/homeworks/HW4.java
+++ src/homeworks/HW4.java
@@ -1,5 +1,7 @@
 package homeworks;
 
+import java.util.Scanner;
+
 public class HW4 {
     public static void main(String[] args) {
         /*     Write an application, that will read two numbers from user
@@ -9,6 +11,22 @@ public class HW4 {
 
         If at least one of given numbers is equal to 0, your application
         should print false.*/
+
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.println("Enter number1");
+        int number1 =scanner.nextInt();
+
+        System.out.println("Enter number2");
+        int number2 = scanner.nextInt();
+
+        if (number1 > 0 && number2 > 0) {
+            System.out.println("True");
+
+        } else {
+            System.out.println("False");
+        }
+
 
 
 
