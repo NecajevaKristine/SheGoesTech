d6fb5cafdd26f8c406e0dd071be5226028a3e459
Kristine Necajeva <kristinenecajeva@inbox.lv>
2022-12-07 21:05:18 +0200

    ChristmasTree.StringExamples

---
+++ src/homeworks/HW10.java
@@ -0,0 +1,9 @@
+package homeworks;
+
+public class HW10 {
+    public static void main(String[] args) {
+
+
+
+    }
+}
--- src/homeworks/HW9.java
+++ src/homeworks/HW9.java
@@ -1,10 +1,37 @@
 package homeworks;
 
+import java.util.Arrays;
+import java.util.Scanner;
+
 public class HW9 {
     public static void main(String[] args) {
 
+        Scanner scanner = new Scanner (System.in);
+
+        System.out.println( "Please enter number for tree size");
+        int treeSize = scanner.nextInt();
+
+        //levels
+            for (int i = 0; i<treeSize; i++) {
+
+                //spaces
+                for (int j = 0; j < treeSize - 1-i; j++) {
+                    System.out.print(" ");
+                }
+                //stars
+                for (int j = 0; j < i * 2 + 1; j++) {
+                    System.out.print("*");
+                }
+                System.out.println();
+
+            }
+
+            for(int i = 0; i<treeSize-1; i++) {
+                System.out.print(" ");
+            }
+
+            System.out.println("#");
 
 
-
-    }
+        }
 }
